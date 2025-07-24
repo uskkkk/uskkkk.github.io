6c7f6d261c74baefc0ca892a77482bb69308db69
uskkkk <dntjd4562@gmail.com>
2025-07-24 17:45:15 +0900

    feat) 햄버거 메뉴 기능 추가

--- uskkkk.github.io/src/components/navigator/Navigator.js
+++ uskkkk.github.io/src/components/navigator/Navigator.js
@@ -1,21 +1,39 @@
+import React, { useState } from 'react';
 import '../../styles/Navigator.css'
 
 function Navigator(props) {
-    return (
-        <div className="navigator-container">
-            <div className='navigator-title'>{props.title}</div>
-            <div className='navigator-menu-box'>
-                <div className='navigator-menu'>{props.home}</div>
-                <div className='navigator-menu'>{props.skill}</div>
-                <div className='navigator-menu'>{props.contact}</div>
-            </div>
-            <div className='navigator-hambuger-toggle'>
-                <div className='hambuger-top-line'></div>
-                <div className='hambuger-middle-line'></div>
-                <div className='hambuger-bottom-line'></div>
-            </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <>
+      <div className="navigator-container">
+        <div className="navigator-title">{props.title}</div>
+
+        <div className={`navigator-menu-box ${isOpen ? 'hidden' : ''}`}>
+          <div className="navigator-menu">{props.home}</div>
+          <div className="navigator-menu">{props.skill}</div>
+          <div className="navigator-menu">{props.contact}</div>
         </div>
-    );
+
+        <div className="navigator-hambuger-toggle" onClick={toggleSidebar}>
+          <div className={isOpen ? 'hambuger-top-line active' : 'hambuger-top-line'}></div>
+          <div className={isOpen ? 'hambuger-middle-line active' : 'hambuger-middle-line'}></div>
+          <div className={isOpen ? 'hambuger-bottom-line active' : 'hambuger-bottom-line'}></div>
+        </div>
+      </div>
+
+      {/* 사이드바 */}
+      <div className={`sidebar ${isOpen ? 'show' : ''}`}>
+        <div className="sidebar-menu">{props.home}</div>
+        <div className="sidebar-menu">{props.skill}</div>
+        <div className="sidebar-menu">{props.contact}</div>
+      </div>
+    </>
+  );
 }
 
 export default Navigator;
