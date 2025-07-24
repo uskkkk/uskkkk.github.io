8e86daa9e3eb60963405b24b5bf6e29d7fa7570a
uskkkk <dntjd4562@gmail.com>
2025-07-24 16:56:22 +0900

    Chore) create navigator

---
+++ uskkkk.github.io/src/components/card/Card.js
@@ -0,0 +1,9 @@
+import '../../styles/Card.css'
+
+function Card() {
+    return (
+        <div className="card-layout"></div>
+    );
+}
+
+export default Card;
---
+++ uskkkk.github.io/src/components/navigator/Navigator.js
@@ -0,0 +1,16 @@
+import '../../styles/Navigator.css'
+
+function Navigator(props) {
+    return (
+        <div className="navigator-container">
+            <div className='navigator-title'>{props.title}</div>
+            <div className='navigator-menu-box'>
+                <div className='navigator-menu'>{props.home}</div>
+                <div className='navigator-menu'>{props.skill}</div>
+                <div className='navigator-menu'>{props.contact}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navigator;
